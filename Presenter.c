53a96dd3d44dbd46fb4e14f2eac4d482d63e2758
soenkek <soenke.krauth@gmx.de>
2019-01-14 20:44:21 +0100

    Karten mischen #2

--- Presenter.c
+++ Presenter.c
@@ -1,4 +1,4 @@
-//#include <time.h>
+#include <time.h>
 #include <stdlib.h>
 #include "View.h"
 #include "data_structures.h"
@@ -16,6 +16,7 @@ int main() {
 	while(1) {
 		validateGameInput();
 	}
+	return 0;
 }
 
 
@@ -26,46 +27,61 @@ void validateGameInput() {
 }
 
 void generateCards() {
+	//initialize cards with defaults
 	for (int i = 0; i < FIELD_SIZE; ++i) {
 		for (int j = 0; j < FIELD_SIZE; ++j)
 		{
-			cards[i][j].image = 66;
+			cards[i][j].image = -1;
 			cards[i][j].animationState = 0;
 			cards[i][j].visibility = 1;
 		}
 	}
-/*
-*	srand(time(NULL));
-*	int numChars = FIELD_SIZE * FIELD_SIZE / 2;
-*	int *chars = (int*) malloc (numChars * sizeof(int));
-*	int i = 0;
-*	while(1) {
-*		for (; i < numChars; ++i)
-*		{
-*			int ascii = rand() / 25 + 65;
-*		}
-*		int lastChar = chars[0];
-*		for (int j = 1; j < numChars; ++j)
-*		{
-*			for (int k = j + 1; k < numChars; ++k)
-*			{
-*				if(chars[j] == chars[k]) {
-*					i = j;
-*					break;
-*				}
-*			}
-*			if (j == numChars - 1)
-*			{
-*				for (int l = 0; l < FIELD_SIZE; ++l)
-*				{
-*					for (int m = 0; m < FIELD_SIZE; ++m)
-*					{
-*						//save num to 2 random places
-*					}
-*				}
-*				return;
-*			}
-*		}
-*	}
-*/
+	//generate random numbers
+	srand(time(NULL));
+	int numChars = FIELD_SIZE * FIELD_SIZE / 2;
+	int *chars = (int*) malloc (numChars * sizeof(int));
+	int i = 0;
+	while(1) {	//generatorLoop
+		for (; i < numChars; ++i)
+		{
+			int ascii = (rand() % 25) + 65;
+			chars[i] = ascii;
+		}
+		//check if unique
+		for (int j = 0; j < numChars; ++j) //outer checkLoop
+		{
+			for (int k = j + 1; k < numChars; ++k) //inner checkLoop
+			{
+				if(chars[j] == chars[k]) {
+					i = j;
+					j = k = numChars; //break out of both checkLoops
+					break;
+				}
+			}
+		}
+		if (i >= numChars)
+		{
+			break; //break out of generatorLoop
+		}
+	}
+	//put numbers into cards
+	for (i = 0; i < numChars; ++i)
+	{
+		printf("%d\n", i);
+		for (int j = 0; j < 2; ++j) //2 cards per image
+		{
+			int position = rand() % FIELD_SIZE * FIELD_SIZE - 1;
+			int x = position/FIELD_SIZE;
+			int y = position%FIELD_SIZE;
+			while(cards[x][y].image != -1) { //iterate until "empty" card is found
+				position++;
+				if (position > FIELD_SIZE * FIELD_SIZE - 1) {
+					position = 0;
+				}
+				x = position/FIELD_SIZE;
+				y = position%FIELD_SIZE;
+			}
+			cards[x][y].image = chars[i];
+		}
+	}
 }
